ac6abc96f549ab29911a1def73d2148f912bf014
Herry Prasetyo <herry081288@gmail.com>
2022-06-05 16:15:03 +0700

    GetALlBOOKID

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,24 @@
+const {
+    addBookHandler,
+    getAllBooksHandler,
+    getBookByIdHandler,
+} = require('./handler');
 const routes = [
     {
-        path: '/',
+        path: '/books',
         method: 'POST',
-        handler: (request, h) => {
-        },
+        handler: addBookHandler
     },
+    {
+        path: '/books',
+        method: 'GET',
+        handler: getAllBooksHandler,
+    },
+    {
+        path: '/books/{id}',
+        method: 'GET',
+        handler: getBookByIdHandler,
+    }
 ];
 
 module.exports = routes;
